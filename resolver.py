2b9e11bbf29bbf4c8a1ef0e71801c8e08e6fe78d
Zhong Wang <zw@z-w.ca>
2023-05-13 03:41:55 -0700

    the code is functional now

--- resolver.py
+++ resolver.py
@@ -1,141 +1,139 @@
+"""
+DNS Resolver
+
+This script resolves a given domain using a specified DNS resolver. It supports DNS over UDP (User Datagram Protocol),
+DNS over TLS (Transport Layer Security), and DNS over HTTPS (Hypertext Transfer Protocol Secure) methods for resolving
+the domain.
+
+Usage:
+    python resolver.py <DNS resolver> <domain>
+
+Arguments:
+    <DNS resolver> (optional):
+        - The DNS resolver to use for resolving the domain. It can be provided as:
+            - An IP address: The script will use UDP to send the DNS query to the specified IP address.
+            - A domain name: The script will use DNS over TLS (DOT) to send the DNS query to the specified domain.
+            - A DOH URL (starting with 'https'): The script will use DNS over HTTPS (DOH) to send the DNS query.
+
+    <domain> (required):
+        - The domain to be resolved. It should be provided as a fully qualified domain name (FQDN).
+
+Examples:
+    python resolver.py 8.8.8.8 example.com
+        - Resolves the domain 'example.com' using the DNS resolver at IP address '8.8.8.8' via UDP.
+
+    python resolver.py dns.example.com example.com
+        - Resolves the domain 'example.com' using the DNS resolver at 'dns.example.com' via DNS over TLS (DOT).
+
+    python resolver.py https://dns.example.com/dns-query example.com
+        - Resolves the domain 'example.com' using the DNS resolver at 'https://dns.example.com/dns-query' via DNS over HTTPS (DOH).
+
+Note:
+    - If only the DNS resolver is not provided, the script will use the system's default resolver
+      and attempt to resolve the domain using UDP.
+
+Dependencies:
+    - The script requires the 'dnspython' library to be installed. You can install it using pip:
+        pip install dnspython
+
+"""
+
 import sys
 import re
-import itertools
 import socket
-import time
-from collections import Counter
-
-import dns.resolver
 import dns.query
-import dns.message
-import urllib.request
-from urllib.parse import urlparse
-# DnsHostname = 'dns.quad9.net'
-#DnsIP='8.8.8.8'
-#DnsHostname='https://1.1.1.2/dns-query'
-# DnsHostname='https://blitz.ahadns.com/1:2.8.20.22'
-# DnsHostname='https://blitz.ahadns.com/1:1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27'
-# DnsIP ='103.247.36.36' # DNSFilter
-#DnsHostname='https://doh.z-w.ca'
-DnsHostname='7ee455.dns.nextdns.io'
+import dns.resolver
+import urllib.parse
 
+def resolve(domain, DnsHostname, DnsIP, QueryMethod):
+    """
+    Resolves the given domain using the specified DNS resolver.
 
+    Args:
+        domain (str): The domain to be resolved.
+        DnsHostname (str): The hostname of the DNS resolver.
+        DnsIP (str): The IP address of the DNS resolver.
+        QueryMethod (str): The method used for DNS resolution (DOT, DOH, or UDP).
 
-def resolve(domain):
-    global DnsHostname
-    global DnsIP
-    global QueryMethod
+    Returns:
+        None
 
-    # Reject if just IP address, not domain
-    if (re.findall('\d+\.\d+\.\d+\.\d+', domain)):
-        return
+    Raises:
+        Exception: If an error occurs during resolution.
 
+    """
     try:
         if QueryMethod == 'DOT':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.tls(dns.message.make_query(dns.name.from_text(
-                domain), dns.rdatatype.A), DnsIP, server_hostname=DnsHostname, timeout=5).to_text())[0]
+                domain), dns.rdatatype.A), DnsIP, server_hostname=DnsHostname, timeout=2).to_text())
 
         elif QueryMethod == 'DOH':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.https(dns.message.make_query(
-                dns.name.from_text(domain), dns.rdatatype.A), DnsHostname, timeout=5).to_text())[0]
+                dns.name.from_text(domain), dns.rdatatype.A), DnsHostname, timeout=2).to_text())
 
         elif QueryMethod == 'UDP':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.udp(dns.message.make_query(
-                dns.name.from_text(domain), dns.rdatatype.A), DnsIP, timeout=5).to_text())[0]
+                dns.name.from_text(domain), dns.rdatatype.A), DnsIP, timeout=2).to_text())
 
-        print("{} with {} on {}: {}".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname, ipaddr))
-        #time.sleep(1)
-        return(ipaddr)
-    except:
-        pass
+        if ipaddr:
+            print("Resolved IP address for domain '{}' using {} on {}: \n{}\n".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname, ipaddr))
+        else:
+            print("Could not resolve an IP address for domain '{}' using {} on {}".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname))
+    except Exception as e:
+        print("Error during resolution: {}".format(e))
 
 
 def setDNS(i):
-    global DnsHostname
-    global DnsIP
+    """
+    Sets the DNS resolver details based on the input.
 
-    # if sys input DNS resolver domain/IP is wrong, just use system resolver
-    try:
-        HostIP = socket.gethostbyname(i)
-    except:
-        print("Input DNS failed. Use Default DNS")
-        return
+    Args:
+        i (str): The input for DNS resolver (either a DOH URL, domain, or IP address).
 
-    DnsIP = HostIP
+    Returns:
+        tuple: A tuple containing the DnsHostname, DnsIP, and QueryMethod.
 
-    # Detect if sys input is IP address for UDP, or domain for DOT/DOH
-    if HostIP == i:
-        try:
-            del DnsHostname
-        except:
-            pass
-    else:
+    """
+    DnsHostname = None
+    DnsIP = None
+    QueryMethod = None
+
+    if i.startswith('https'):  # Input is a DOH URL
         DnsHostname = i
-
-
-def main():
-    # Set DNS from sys input (if exists)
-    if len(sys.argv) >= 2:
-        server = sys.argv[1]
-        domain = sys.argv[2]
+        QueryMethod = "DOH"
     else:
-        print("Input: dns-server, domain")
-        exit()
-
-    setDNS(i)
+        try:
+            DnsIP = socket.gethostbyname(i)
+            if DnsIP == i:  # Input is an IP address
+                QueryMethod = "UDP"
+            else:  # Input is a domain
+                DnsHostname = i
+                QueryMethod = "DOT"
+        except:
+            print("Input DNS failed. Using Default DNS")
+            DnsIP = socket.gethostbyname(socket.gethostname())  # Default DNS
+            QueryMethod = "UDP"  # Default method
     
-
-    # If DNS IP not defined earlier or sys input, use system default resolver
-    try:
-        DnsIP
-    except NameError:
-        dnsresolver = dns.resolver.Resolver()
-        DnsIP = dnsresolver.nameservers[0]
-
-    # Final try: check if there is a defined DOT/DOH resolver. After that, set DNS query method
-    try:
-        DnsHostname
-    except NameError:
-        QueryMethod = "UDP"
-        print("UDP: {}".format(DnsIP))
-    else:
-        DnsIP = socket.gethostbyname(urllib.parse.urlparse(DnsHostname).netloc if bool(re.search("^https", DnsHostname)) else DnsHostname)
-
-        if bool(re.search('^https', DnsHostname)):
-            QueryMethod = "DOH"
-            print("DOH: {}".format(DnsHostname))
-        else:
-            QueryMethod = "DOT"
-            print("DOT: {},{}".format(DnsHostname, DnsIP))
-
-    # Add DNS IP to blocked IP
-    BlockedIPs.append(DnsIP)
-
-    # Read test url/domain list and remove duplicates
-    data = list(set(itertools.chain.from_iterable(
-        [urllib.request.urlopen(i).readlines() for i in feedurl])))
-
-    # Perform DNS test. If possible, resolve domains in parallel. Also parse link into domain
-    try:
-        import concurrent.futures
-    except ImportError:
-        print("Single Thread ...")
-        result = [resolve(urlparse(line.strip()).netloc.decode() if re.search("http", line.decode()) else line.decode().strip()) for line in data]
-    else:
-        print("Multiple Thread ...")
-        #result = Parallel(n_jobs=10)(delayed(resolve)(urlparse(line.strip()).netloc.decode() if re.search("http", line.decode()) else line.decode().strip()) for line in data)
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            future_results = {executor.submit(resolve, urlparse(line.strip()).netloc.decode() if re.search("http", line.decode()) else line.decode().strip()): line for line in data}
-            result = []
-            for future in concurrent.futures.as_completed(future_results):
-                result.append(future.result())
-
-
+    return DnsHostname, DnsIP, QueryMethod
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        # If no command-line arguments provided
+        print("Please provide the DNS resolver and domain to resolve as command line arguments.")
+        sys.exit(1)
+    elif len(sys.argv) < 3:
+        # Only the DNS resolver provided, use system default resolver
+        domain = sys.argv[1]
+        DnsIP = dns.resolver.Resolver().nameservers[-1]
+        DnsHostname = None
+        QueryMethod = "UDP"
+    elif len(sys.argv) == 3: 
+        # DNS resolver and domain provided
+        DnsHostname, DnsIP, QueryMethod = setDNS(sys.argv[1])
+        domain = sys.argv[2]
+
+    resolve(domain, DnsHostname, DnsIP, QueryMethod)
 
 
-
+    
