c8ce22d66d4a618b55e28e09888bda4c8050d7cb
Zhong Wang <zw@z-w.ca>
2023-05-13 04:19:37 -0700

    add ping

--- resolver.py
+++ resolver.py
@@ -38,12 +38,15 @@ Dependencies:
 
 """
 
+import subprocess
 import sys
 import re
 import socket
 import dns.query
 import dns.resolver
 import urllib.parse
+from concurrent.futures import ThreadPoolExecutor
+
 
 def resolve(domain, DnsHostname, DnsIP, QueryMethod):
     """
@@ -65,23 +68,56 @@ def resolve(domain, DnsHostname, DnsIP, QueryMethod):
     try:
         if QueryMethod == 'DOT':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.tls(dns.message.make_query(dns.name.from_text(
-                domain), dns.rdatatype.A), DnsIP, server_hostname=DnsHostname, timeout=2).to_text())
+                domain), dns.rdatatype.A), DnsIP, server_hostname=DnsHostname, timeout=5).to_text())
 
         elif QueryMethod == 'DOH':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.https(dns.message.make_query(
-                dns.name.from_text(domain), dns.rdatatype.A), DnsHostname, timeout=2).to_text())
+                dns.name.from_text(domain), dns.rdatatype.A), DnsHostname, timeout=5).to_text())
 
         elif QueryMethod == 'UDP':
             ipaddr = re.findall('\d+\.\d+\.\d+\.\d+', dns.query.udp(dns.message.make_query(
-                dns.name.from_text(domain), dns.rdatatype.A), DnsIP, timeout=2).to_text())
+                dns.name.from_text(domain), dns.rdatatype.A), DnsIP, timeout=5).to_text())
 
         if ipaddr:
-            print("Resolved IP address for domain '{}' using {} on {}: \n{}\n".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname, ipaddr))
+            #print("Resolved IP address for domain '{}' using {} on {}: \n{}".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname, ipaddr))
+            print("Resolved IP address for domain '{}' using {} on {}: ".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname))
+            
+            # Create a thread pool executor with a maximum number of threads
+            with ThreadPoolExecutor() as executor:
+                # Submit ping tasks to the executor
+                ping_tasks = [executor.submit(ping_ip, ip) for ip in ipaddr]
+                
+                # Retrieve the results from the completed tasks
+                for task, ip in zip(ping_tasks, ipaddr):
+                    ping_ms = task.result()
+                    print("{} (Ping: {} ms)".format(ip, ping_ms))
+
         else:
             print("Could not resolve an IP address for domain '{}' using {} on {}".format(domain, QueryMethod, DnsIP if QueryMethod == 'UDP' else DnsHostname))
     except Exception as e:
         print("Error during resolution: {}".format(e))
 
+def ping_ip(ip):
+    try:
+        # Execute the ping command once and capture the output
+        if sys.platform.startswith('win'):
+            command = ['ping', '-n', '1', ip]  # Windows uses -n option to specify number of pings
+        else:
+            command = ['ping', '-c', '1', ip]  # Unix-like systems use -c option to specify number of pings
+
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        # Parse the output to retrieve the latency (ms)
+        output_lines = result.stdout.strip().split('\n')
+        for line in output_lines:
+            if 'time=' in line:
+                latency = re.findall(r'time=(\d+\.?\d*)', line)
+                if latency:
+                    return latency[0]
+        return "N/A"  # If ping failed or latency not found
+    except:
+        return "N/A"  # If ping execution failed
+    
 
 def setDNS(i):
     """
@@ -115,6 +151,8 @@ def setDNS(i):
             QueryMethod = "UDP"  # Default method
     
     return DnsHostname, DnsIP, QueryMethod
+
+
 
 
 if __name__ == '__main__':
